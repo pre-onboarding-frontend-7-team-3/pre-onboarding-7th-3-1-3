66a77fc83e43c22686b84a5f0ac31a67191f3213
Sangmin <23mean@naver.com>
2022-11-09 18:54:30 +0900

    feat: 동작과정 기본 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import { ThemeProvider } from "styled-components";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Theme from "styles/theme";
 
+import Home from "pages/home";
+
 function App() {
-  return <ThemeProvider theme={Theme}>app</ThemeProvider>;
+  return (
+    <ThemeProvider theme={Theme}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/utils/isMakeSense.ts
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+const isMakeSense = (searchTarget: string) => {
+  const korean = /[ㄱ-ㅎ|ㅏ-ㅣ]/;
+
+  return !korean.test(searchTarget) && searchTarget !== "";
+};
+
+export default isMakeSense;
