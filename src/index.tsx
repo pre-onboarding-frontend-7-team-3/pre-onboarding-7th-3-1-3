2a81ba9b36fd07e514aa19342affe27dc6c88f1b
jungmin <qkrwjdals302@naver.com>
2022-11-09 16:44:52 +0900

    Feat: msw 초기 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import App from "App";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { worker } from "./mocks/worker";
+
+if (process.env.NODE_ENV === "development") {
+  worker.start();
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(<App />);
