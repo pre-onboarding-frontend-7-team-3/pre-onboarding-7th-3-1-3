de3f9518cfafd0a81f0110e2a21d0756899ce7fa
YeonghunKO <yhko1988@gmail.com>
2022-11-09 15:34:15 +0900

    feat : App에 Home 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { ThemeProvider } from "styled-components";
 import Theme from "styles/theme";
+import Home from "pages/Home";
+import GlobalStyle from "styles/GlobalStyle";
+import Layout from "components/Layout";
 
 function App() {
-  return <ThemeProvider theme={Theme}>appS</ThemeProvider>;
+  return (
+    <ThemeProvider theme={Theme}>
+      <GlobalStyle />
+      <Layout>
+        <Home />
+      </Layout>
+    </ThemeProvider>
+  );
 }
 
 export default App;
