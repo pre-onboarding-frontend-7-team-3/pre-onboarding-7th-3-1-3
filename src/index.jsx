757b17676b9944ecf5f1aa3ac091d26ebc1e146b
PiperChang <goddhqj8000@gmail.com>
2022-11-10 23:22:38 +0900

    Fix: theme export lint error

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import GlobalStyle from "./style/GlobalStyle";
 import { ThemeProvider } from "styled-components";
-import theme from "./style/Theme";
+import { theme } from "./style/Theme";
 import { RecoilRoot } from "recoil";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
