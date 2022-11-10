d9166190094fd81b1db56f4771a7efb04abfea7c
Sangmin <23mean@naver.com>
2022-11-10 23:36:55 +0900

    fix: header title 줄 구분

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,15 @@
 import styled from "styled-components";
 
 const Header = () => {
-  const HEADING = "국내 모든 임상시험 검색하고 온라인으로 참여하기";
+  const HEADING_FRONT = "국내 모든 임상시험 검색하고";
+  const HEADING_BACK = "온라인으로 참여하기";
 
-  return <HeaderWrapper>{HEADING}</HeaderWrapper>;
+  return (
+    <HeaderWrapper>
+      <div>{HEADING_FRONT}</div>
+      <div>{HEADING_BACK}</div>
+    </HeaderWrapper>
+  );
 };
 
 export default Header;
