2a9b21acb2cc0375e464680f978f8a64fb829c6c
박정민 <55550034+ono212@users.noreply.github.com>
2022-11-10 20:45:35 +0900

    [ Refactor ] 추천검색어 리스트에 스크롤 생길 시 방향키로 이동가능하도록 구현 (#6)
    * Feat: 추천검색어 리스트에 스크롤 생길 시 방향키로 이동가능하도록 구현
    - SearchItemList에 max-height 지정
    - useKeyDown파일 빈 라인 린트오류로 삭제
    
    * Refactor: useEffect사용 및 isSelected변수로 따로 생성

--- src/components/Main/SearchItem.jsx
+++ src/components/Main/SearchItem.jsx
@@ -1,13 +1,22 @@
 import { useRecoilValue } from "recoil";
+import { useEffect, useRef } from "react";
 import styled from "styled-components";
 import { selectedSearchResultIndex } from "../../store/selectedSearchResultIndex";
 import formatFontWeight from "../../utils/formatFontWeight";
 
 const SearchItem = ({ idx, sickNm, searchInputValue }) => {
   const selectedIndex = useRecoilValue(selectedSearchResultIndex);
+  const selected = useRef();
+
+  useEffect(() => {
+    const isSelected = selectedIndex === idx;
+    if (isSelected) {
+      selected.current.scrollIntoView({ behavior: "smooth", block: "nearest", inline: "start" });
+    }
+  }, [selectedIndex, idx]);
 
   return (
-    <Item selectedIndex={selectedIndex} idx={idx}>
+    <Item selectedIndex={selectedIndex} idx={idx} ref={selected}>
       {formatFontWeight(sickNm, searchInputValue)}
     </Item>
   );
