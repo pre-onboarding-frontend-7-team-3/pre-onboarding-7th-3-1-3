c8cf7802587d41af62d7b3a756a9ca89de0c0dfb
moonkorea00 <moonkorea00@gmail.com>
2022-11-11 00:13:44 +0900

    fix : server url

--- src/apis/getDataAndRegisterCache.ts
+++ src/apis/getDataAndRegisterCache.ts
@@ -1,17 +1,17 @@
-import type { ResultData } from "../store/searchResult";
-
-const getDataAndRegisterCache = (searchTarget: string): Promise<ResultData[]> => {
-  return fetch(`http://localhost:4000/sick?sickNm_like=${searchTarget}`, {
+const getDataAndRegisterCache = (searchTarget: string): Promise<object[]> => {
+  return fetch(`${process.env.REACT_APP_BASE_URL}/sick?sickNm_like=${searchTarget}`, {
     method: "get",
   }).then((fetchRes) => {
     let responseClone = fetchRes.clone();
     caches.open("search").then((cache) => {
-      cache.put(`http://localhost:4000/sick?sickNm_like=${searchTarget}`, responseClone);
+      cache.put(
+        `${process.env.REACT_APP_BASE_URL}/sick?sickNm_like=${searchTarget}`,
+        responseClone
+      );
     });
 
     // eslint-disable-next-line no-console
-    console.info("calling api");
-
+    console.log("api calling");
     return fetchRes.json();
   });
 };
